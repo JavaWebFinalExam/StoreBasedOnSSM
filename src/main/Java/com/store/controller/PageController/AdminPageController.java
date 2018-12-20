7631c37c2631813a0bbb29d958be8aacc5ffc276
danchun <danchunshi@qq.com>
2018-12-20 16:00:33 +0800

    add some class

---
+++ src/main/Java/com/store/controller/PageController/AdminPageController.java
@@ -0,0 +1,15 @@
+package com.store.controller.PageController;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 主要为管理员页面控制
+ * 主要为石立军部分
+ */
+@Controller
+@RequestMapping("/adminPage")
+public class AdminPageController {
+
+}
---
+++ src/main/Java/com/store/controller/PageController/OrderPageController.java
@@ -0,0 +1,14 @@
+package com.store.controller.PageController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 订单、购物车部分页面控制
+ * 主要为陈俊卿部分
+ */
+@Controller
+@RequestMapping("/userPage/ordAndCart")
+public class OrderPageController {
+
+}
