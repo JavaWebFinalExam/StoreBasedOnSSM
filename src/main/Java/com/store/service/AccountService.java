208f5b451bc839779893db63ca420b38e5d0e082
DanChunStone <1912504224@qq.com>
2018-12-21 10:16:44 +0800

    fixed some bug

--- src/main/Java/com/store/service/AccountService.java
+++ src/main/Java/com/store/service/AccountService.java
@@ -5,8 +5,12 @@ import com.store.entity.Account;
 import java.util.List;
 
 public interface AccountService {
+    //获取所有用户
     List<Account> getAllAccount();
+    //根据id删除用户
     boolean deleteUserById(int id);
+    //验证登录
     boolean checkLogin(String username,String password);
+    //注册
     boolean register(String username, String password);
 }
