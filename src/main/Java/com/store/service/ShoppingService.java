c344c078bf304a13c5be2559ffde8f50403403ee
Digimonseeker <1741199512@qq.com>
2018-12-23 14:43:24 +0800

    addtoshoppingcart,showeevaluation

--- src/main/Java/com/store/service/ShoppingService.java
+++ src/main/Java/com/store/service/ShoppingService.java
@@ -1,5 +1,15 @@
 package com.store.service;
 
 
+import com.store.entity.Shoppingcart;
+import org.apache.ibatis.annotations.Param;
+
 public interface ShoppingService {
+
+    //将商品加入购物车
+    void addProductToShoppingCart(int id, int productId,int userId);
+
+    //根据用户ID来返回购物车
+    Shoppingcart selectByUserId(Integer userId);
+
 }
