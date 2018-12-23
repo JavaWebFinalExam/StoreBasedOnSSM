752a3b342659870475381a9e4533dab2caee8a18
DanChunStone <1912504224@qq.com>
2018-12-23 22:28:42 +0800

    add postController class

---
+++ src/main/Java/com/store/controller/PostController.java
@@ -0,0 +1,30 @@
+package com.store.controller;
+
+import com.store.entity.Order;
+import com.store.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/Post")
+public class PostController {
+    @Autowired
+    PostService postService;
+
+    @RequestMapping(value = "/deletePostById",method = RequestMethod.POST,produces = "application/json;charset=UTF-8")
+    @ResponseBody
+    public Map<String,Object> deletePostById(@RequestBody Map<String,Object> map){
+        Map<String,Object> ResponseMap = new HashMap<>();
+
+        
+
+        return ResponseMap;
+    }
+}
